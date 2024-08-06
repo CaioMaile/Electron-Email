c169bcbecc0c60c9360eba271aa59d165140b976
CaioMaile <caiomailesla@gmail.com>
2024-08-06 14:47:49 -0300

    finalização do projeto

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-const { app, BrowserWindow } = require("electron")
+const { app, BrowserWindow, ipcMain } = require("electron")
 const {join} = require("path")
+const EnviarEmail = require("./functions/EnviarEmail")
 app.whenReady()   
     .then(() => {
+        ipcMain.on("EnviarEmail", EnviarEmail)
         const janela = new BrowserWindow({
             autoHideMenuBar: true,
             height: 600,
